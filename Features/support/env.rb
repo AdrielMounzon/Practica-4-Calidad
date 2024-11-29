65766738e683b9b30f79d738f9d2a128374c0a6c
Adriel Mounzón <adriel.mounzon@ucb.edu.bo>
2024-11-29 12:24:34 -0400

    Se crea la carpeta de reportes con un reporte de ejemplo

--- Features/support/env.rb
+++ Features/support/env.rb
@@ -6,12 +6,13 @@ begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations
     
     
     Capybara.default_driver = :selenium
+    Capybara.save_path = File.join(Dir.pwd, 'reports')
     
     # Set the host the Capybara tests should be run against
     Capybara.app_host = ENV["CAPYBARA_HOST"]
     
     # Set the time (in seconds) Capybara should wait for elements to appear on the page
-    Capybara.default_max_wait_time = 15
+    Capybara.default_max_wait_time = 5
     Capybara.default_driver = :selenium
     Capybara.app_host = "https://demo.borland.com/gmopost/"
     
