ed422a70d7d7b1eeab5872c27f9275954c4c3cb0
Adrian Montano <adrian.montano2011@yahoo.com>
2024-12-01 18:05:50 -0400

    Primer scenario

--- features/billing.feature
+++ features/billing.feature
@@ -3,24 +3,23 @@ Feature: Fill in personal information
   I want to provide my information
   So that I can pay and recieve my order
 
-  Scenario: Fill in all fields with correct data
+  @maximize
+  Scenario: Fill out Bill To information correctly
     Given I am on the "Billing Information" page
-    And I fill in the "<field name>" with "<field value>"
+    And I fill in the Bill To form with the following values:
+      | Name        | Francisco           |
+      | Address     | Linden Ave #445     |
+      | City        | Las Vegas           |
+      | State       | Nevada              |
+      | Zip         | 89110               |
+      | Phone       | 1234567890          |
+      | E-mail      | francisco@gmail.com |
+      | Credit Card | MasterCard          |
+      | Card Number | 1234-5678-1234-5678 |
+      | Card Date   | 11/26               |
     And I check the Same as "Bill to" option
     When I click the "Place The Order" button
     Then the "OnLine Store Receipt" page should show all the information I provided
-
-    | field name  | field value         |
-    | Name        | Francisco           |
-    | Address     | Linden Ave #445     |
-    | City        | Las Vegas           |
-    | State       | Nevada              |
-    | Zip         | 89110               |
-    | Phone       | 1234567890          |
-    | E-mail      | francisco@gmail.com |
-    | Credit Card | MasterCard          |
-    | Card Number | 1234-5678-1234-5678 |
-    | Expiration  | 10/26               |
 
   Scenario Outline: Leave a required field empty
   Scenario: Enter a non valid zip code
---
+++ features/step_definitions/billing_steps.rb
@@ -0,0 +1,50 @@
+# Given I am on the "Billing Information" page
+Given('I am on the "Billing Information" page') do
+    visit('https://demo.borland.com/gmopost/cgi-bin/perl.exe?ship-info.pl')
+  end
+
+# And I fill in the Bill To form with the following values:
+When('I fill in the Bill To form with the following values:') do |table|
+    data = table.rows_hash
+    data.each_pair do |key, value|
+    case key
+    when "Name"
+        fill_in 'billName', :with => value
+        @billName = value
+    when "Address"
+        fill_in 'billAddress', :with => value
+        @billAddress = value
+    when "City"
+        fill_in 'billCity', :with => value
+        @billCity = value
+    when "State"
+        fill_in 'billState', :with => value
+        @billState = value
+    when "Zip"
+        fill_in 'billZipCode', :with => value
+        @billZipCode = value
+    when "Phone"
+        fill_in 'billPhone', :with => value
+        @billPhone = value
+    when "E-mail"
+        fill_in 'billEmail', :with => value
+        @billEmail = value
+    when "Credit Card"
+        select value, from: 'CardType'
+        @CardType = value
+    when "Card Number"
+        fill_in 'CardNumber', :with => value
+        @CardNumber = value
+    when "Card Date"
+        fill_in 'CardDate', :with => value
+        @CardDate = value
+    end
+    end
+end
+
+# And I check the Same as "Bill to" option
+When('I check the Same as "Bill to" option') do
+    find(:xpath, '/html/body/form/table/tbody/tr[2]/td[3]/table/tbody/tr[1]/td[2]/input').set(true)
+end
+
+# Then the "OnLine Store Receipt" page should show all the information I provided
