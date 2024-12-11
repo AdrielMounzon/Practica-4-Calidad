35cdf4b58efae7940830c0bff44c36a52bc6bf99
Osber <osberriojalopez@gmail.com>
2024-12-10 20:04:06 -0400

    pop shopping_cart

--- features/step_definitions/shopping_cart_steps.rb
+++ features/step_definitions/shopping_cart_steps.rb
@@ -1,135 +1,53 @@
-# Given I am on the "OnLine Catalog" page
+# shopping_cart_steps.rb
 Given('I am on the "OnLine Catalog" page') do
-  visit('https://demo.borland.com/gmopost/online-catalog.htm')
+  @shopping_cart_page = ShoppingCartPage.new
+  @shopping_cart_page.visit_catalog
 end
 
-# And I click on the "<product_name>" item on the item name column
 Given('I click on the {string} item on the item name column') do |product_name|
-  find(:xpath, "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]").click
+  @shopping_cart_page.select_product(product_name)
 end
 
-# And I am now on the "Products" page
 Given('I am now on the "Products" page') do
-  expect(page.current_url).to match(/https:\/\/demo\.borland\.com\/gmopost\/products\.htm/)
+  @shopping_cart_page.verify_product_page
 end
 
-# And the product "<product_name>" has at least 1 unit
 Given('the product {string} has at least 1 unit') do |product_name|
-  stock_xpath = "//h2/a[contains(text(), '#{product_name}')]/../following-sibling::div[1]//table//tr[2]/td[2]"
-  stock = find(:xpath, stock_xpath).text.strip.to_i
-  expect(stock).to be >= 1
+  @shopping_cart_page.verify_product_stock(product_name, 1)
 end
 
-# And I press the browser's back button
 Given('I press the browser\'s back button') do
   page.go_back
 end
 
-# When I write "1" on the order quantity of "<product_name>"
-When('I write "1" on the order quantity of {string}') do |product_name|
-  input_xpath = "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]/ancestor::tr/td[4]/h1/input"
-  find(:xpath, input_xpath).set(1)
+When('I write "{string}" on the order quantity of {string}') do |quantity, product_name|
+  @shopping_cart_page.set_order_quantity(product_name, quantity)
 end
 
-# And I click the "Place An Order" button
 When('I click the {string} button') do |button_name|
-  click_button(button_name)
+  @shopping_cart_page.place_order
 end
 
-# Then I should be on the "Place Order" page
 Then('I should be on the "Place Order" page') do
-  expect(page.current_url).to match('https://demo.borland.com/gmopost/cgi-bin/perl.exe?place-order.pl')
+  @shopping_cart_page.verify_order_page
 end
 
-# And the cart should display "1" in the quantity column and "<product_name>" in the product description column
-Then('the cart should display "1" in the quantity column and {string} in the product description column') do |product_name|
-  input_xpath = "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]"
-  actual_product_name = find(:xpath, input_xpath).text.strip
-  expect(actual_product_name).to eq(product_name)
+Then('the cart should display "{string}" in the quantity column and {string} in the product description column') do |quantity, product_name|
+  @shopping_cart_page.verify_cart_quantity(product_name, quantity)
 end
 
-# And the product total should be "<product_total>"
 Then('the product total should be {string}') do |product_total|
-  total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[3]/td[3]"
-  expect(find(:xpath, total_xpath).text.strip.gsub('$', '').strip).to eq(product_total)
+  @shopping_cart_page.verify_product_total(product_total)
 end
 
-# And the grand total should be "<grand_total>"
 Then('the grand total should be {string}') do |grand_total|
-  grand_total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[6]/td[2]/strong"
-  expect(find(:xpath, grand_total_xpath).text.strip.gsub('$', '').strip).to eq(grand_total)
+  @shopping_cart_page.verify_grand_total(grand_total)
 end
 
-# Then I should see a message "Please Order Something First"
 Then('I should see a message {string}') do |message_text|
-  expect(page.driver.browser.switch_to.alert.text).to eq(message_text)
+  @shopping_cart_page.verify_alert_message(message_text)
 end
 
-# Then the order form should be cleared
 Then('the order form should be cleared') do
-  input_xpaths = [
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[2]/td[4]/h1/input",
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[3]/td[4]/h1/input",
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[4]/td[4]/h1/input",
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[5]/td[4]/h1/input",
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[6]/td[4]/h1/input",
-    "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[7]/td[4]/h1/input"
-  ]
-
-  input_xpaths.each do |xpath|
-    actual_value = find(:xpath, xpath).value
-    expect(actual_value).to eq('0'), "Expected input at #{xpath} to be '0', but got '#{actual_value}'"
-  end
+  @shopping_cart_page.clear_order_form
 end
-
-# And the product "Padded Socks" has 47 units in stock
-Given('the product {string} has 47 units in stock') do |product_name|
-  stock_xpath = "//h2/a[contains(text(), '#{product_name}')]/../following-sibling::div[1]//table//tr[2]/td[2]"
-  stock = find(:xpath, stock_xpath).text.strip.to_i
-  expect(stock).to eq(47)
-end
-
-# And I am now on the "OnLine Catalog" page
-When ('I am now on the "OnLine Catalog" page') do
-  expect(page.current_url).to match('https://demo.borland.com/gmopost/online-catalog.htm')
-end
-
-# And I write "50" on the order quantity of "Padded Socks"
-When('I write "50" on the order quantity of "Padded Socks"') do
-  input_xpath = "//table//tr//td/a/strong[contains(normalize-space(text()), 'Padded Socks')]/ancestor::tr/td[4]/h1/input"
-  find(:xpath, input_xpath).set(50)
-end
-
-# And the combined product total should be "129.89"
-Then('the combined product total should be {string}') do |product_total|
-  total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[4]/td[3]"
-  expect(find(:xpath, total_xpath).text.strip.gsub('$', '').strip).to eq(product_total)
-end
-
-# And the combined grand total should be "141.38"
-Then('the combined grand total should be {string}') do |grand_total|
-  grand_total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[7]/td[2]/strong"
-  expect(find(:xpath, grand_total_xpath).text.strip.gsub('$', '').strip).to eq(grand_total)
-end
-
-# Then I see the correct Sales Tax as "<sales_tax>"
-Then('I see the correct Sales Tax as {string}') do |sales_tax|
-  sales_tax_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[4]/td[2]"
-  actual_sales_tax = find(:xpath, sales_tax_xpath).text.strip.gsub('$', '').strip
-  expect(actual_sales_tax).to eq(sales_tax), "Expected Sales Tax to be '#{sales_tax}', but got '#{actual_sales_tax}'"
-end
-
-# And the grand total should be "<grand_total>"
-Then('the grand total should be {string}') do |grand_total|
-  grand_total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[6]/td[2]/strong"
-  actual_grand_total = find(:xpath, grand_total_xpath).text.strip.gsub('$', '').strip
-  expect(actual_grand_total).to eq(grand_total), "Expected Grand Total to be '#{grand_total}', but got '#{actual_grand_total}'"
-end
-
-# Then I see the Shipping & Handling as "$5.00"
-Then('I see the Shipping & Handling as {string}') do |shipping_handling|
-  shipping_handling_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[6]/td[2]"
-  actual_shipping_handling = find(:xpath, shipping_handling_xpath).text.strip
-  expect(actual_shipping_handling).to eq(shipping_handling), "Expected Shipping & Handling to be '#{shipping_handling}', but got '#{actual_shipping_handling}'"
-end
-
---
+++ features/support/pages/shopping_cart_page.rb
@@ -0,0 +1,74 @@
+# shopping_cart_page.rb
+class ShoppingCartPage
+    include Capybara::DSL
+  
+    def visit_catalog
+      visit('https://demo.borland.com/gmopost/online-catalog.htm')
+    end
+  
+    def select_product(product_name)
+      find(:xpath, "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]").click
+    end
+  
+    def verify_product_page(product_name)
+      expect(page.current_url).to match(/https:\/\/demo\.borland\.com\/gmopost\/products\.htm/)
+    end
+  
+    def verify_product_stock(product_name, min_stock = 1)
+      stock_xpath = "//h2/a[contains(text(), '#{product_name}')]/../following-sibling::div[1]//table//tr[2]/td[2]"
+      stock = find(:xpath, stock_xpath).text.strip.to_i
+      expect(stock).to be >= min_stock
+    end
+  
+    def set_order_quantity(product_name, quantity)
+      input_xpath = "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]/ancestor::tr/td[4]/h1/input"
+      find(:xpath, input_xpath).set(quantity)
+    end
+  
+    def place_order
+      click_button('Place An Order')
+    end
+  
+    def verify_order_page
+      expect(page.current_url).to match('https://demo.borland.com/gmopost/cgi-bin/perl.exe?place-order.pl')
+    end
+  
+    def verify_cart_quantity(product_name, expected_quantity)
+      input_xpath = "//table//tr//td/a/strong[contains(normalize-space(text()), '#{product_name}')]"
+      actual_product_name = find(:xpath, input_xpath).text.strip
+      expect(actual_product_name).to eq(product_name)
+    end
+  
+    def verify_product_total(expected_total)
+      total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[3]/td[3]"
+      actual_total = find(:xpath, total_xpath).text.strip.gsub('$', '').strip
+      expect(actual_total).to eq(expected_total)
+    end
+  
+    def verify_grand_total(expected_grand_total)
+      grand_total_xpath = "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[6]/td[2]/strong"
+      actual_grand_total = find(:xpath, grand_total_xpath).text.strip.gsub('$', '').strip
+      expect(actual_grand_total).to eq(expected_grand_total)
+    end
+  
+    def clear_order_form
+      input_xpaths = [
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[2]/td[4]/h1/input",
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[3]/td[4]/h1/input",
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[4]/td[4]/h1/input",
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[5]/td[4]/h1/input",
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[6]/td[4]/h1/input",
+        "/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[7]/td[4]/h1/input"
+      ]
+  
+      input_xpaths.each do |xpath|
+        actual_value = find(:xpath, xpath).value
+        expect(actual_value).to eq('0'), "Expected input at #{xpath} to be '0', but got '#{actual_value}'"
+      end
+    end
+  
+    def verify_alert_message(expected_message)
+      expect(page.driver.browser.switch_to.alert.text).to eq(expected_message)
+    end
+  end
+  
